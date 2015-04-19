cfee020b397a55451053ff76ccecc62468d800a2
Jlsilang <Justinlimsilang@gmail.com>
2015-04-19 11:31:27 -0400

    Uploading our code of success
    I'm very glad

---
+++ HackRU_Project/Assets/BallScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+	
+	public Rigidbody2D rb;
+	float constantSpeed = 8.8f;
+	bool finishedDragging = false;
+	bool dragged = false;
+	Vector3 startingPosition;
+	Vector3 endingPosition;
+
+	void Start(){
+
+		//rb = GetComponent<Rigidbody2D> ();
+		startingPosition= rb.position;
+	
+
+	}
+
+	void Update() { 
+
+		if (finishedDragging == true) {
+			rb.velocity = constantSpeed * (rb.velocity.normalized);
+		}
+
+		BoundChecker ();
+
+	}
+
+	void OnMouseDrag(){
+		dragged = true;
+		endingPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		rb.position = endingPosition;
+
+	}
+
+	void OnMouseUp(){
+		if (dragged == true) {
+			finishedDragging = true;
+
+			Vector3 diff = (endingPosition - startingPosition).normalized;
+			rb.velocity = diff;
+		}
+	}
+
+	void BoundChecker(){
+		if(rb.position.x>1.75 || rb.position.x<-1.75 || rb.position.y >2.7 || rb.position.y<-2.7){
+			rb.position = startingPosition;
+			rb.velocity = new Vector3(0, 0, 0);
+			ScoreScript.score = 0;
+		}
+	}
+
+}
---
+++ HackRU_Project/Assets/ScoreScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	public static int score = 0;
+	public static int highscore = 0;
+	// Use this for initialization
+	public Text scoreboard;
+	public Text highscoreboard;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "Green_Wall" || coll.gameObject.name == "Yellow Wall 2" || 
+		    coll.gameObject.name == "Yellow Wall" ) {
+			score++;
+			if(score>highscore){
+				highscore = score;
+			}
+		}
+		
+	}
+
+	void Update(){
+		scoreboard.text ="Bounces: " + score;
+		highscoreboard.text = "Highscore: " + highscore;
+	}
+
+
+}
